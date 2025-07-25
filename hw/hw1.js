1403d82e3e6457e020685bb6ba0a56fe6433e2c2
Dmytro Korotchenko <dvk502@gmail.com>
2025-07-25 14:28:12 +0300

    Update hw1.js

--- hw/hw1.js
+++ hw/hw1.js
@@ -51,6 +51,14 @@ console.log('item_7 == ' + item_7 + '; item_7_type == ' + typeof item_7_type);
 Напишите скрипт, который принимает целое положительное число n (одно любое число от 1 до 9), и выводит в консоль сумму равную 
 n + nn + nnn, где n не перемножаются, а конкатенируются
 */
-const a = 3;
-console.log(a + '' + (a + a) + '' + (a + a + a)); //var 1
-console.log(a + String(a + a) + String(a + a + a)); //var 2
+const n = 9;
+//var1
+const nn = Number(String(n) + String(n));
+const nnn = Number(String(n) + String(n) + String(n));
+console.log(n + nn + nnn);
+//var2
+const mm = Number('' + n + n);
+const mmm = Number('' + n + n + n);
+console.log(n + mm + mmm);
+//var3
+console.log(n + Number('' + n + n) + Number('' + n + n + n));
